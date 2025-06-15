2c5ff4467babcfd59fc47daae16de6c5b9e174ea
Haruyuki Iida <haru.ii.a9i1e@gmail.com>
2025-06-15 04:42:18 +0000

    initial commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_version_footer do
+  name 'Redmine Version Footer plugin'
+  author 'Haruyuki Iida'
+  description 'Show the Redmine version in the footer.'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
