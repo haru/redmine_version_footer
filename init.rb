e4e7dcf8e4488e526437247d1208b547accc28d6
Haruyuki Iida <haru.ii.a9i1e@gmail.com>
2025-06-15 04:52:41 +0000

    remove coverage

--- init.rb
+++ init.rb
@@ -1,3 +1,5 @@
+$LOAD_PATH.unshift "#{File.dirname(__FILE__)}/lib"
+require 'version_footer/view_hook'
 Redmine::Plugin.register :redmine_version_footer do
   name 'Redmine Version Footer plugin'
   author 'Haruyuki Iida'
