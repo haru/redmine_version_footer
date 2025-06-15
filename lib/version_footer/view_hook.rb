d0615ab477e9a17aaf7305f9fe9c28482df16165
Haruyuki Iida <haru.ii.a9i1e@gmail.com>
2025-06-15 05:11:50 +0000

    add version to footer

--- lib/version_footer/view_hook.rb
+++ lib/version_footer/view_hook.rb
@@ -1,5 +1,5 @@
 module VersionFooter
   class ViewHook < Redmine::Hook::ViewListener
-    render_on :view_layouts_base_body_bottom, partial: 'version_footer/view_hook'
+    render_on :view_layouts_base_body_top, partial: 'version_footer/view_hook'
   end
 end
